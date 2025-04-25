e9345e6db193c484f1f28457e9e4438ad62b7b76
Wendell <wendellbaresi93@gmail.com>
2025-04-25 14:52:50 -0300

    feat: Adiciona metodo logar_acao

--- SistemaRH.py
+++ SistemaRH.py
@@ -1,4 +1,10 @@
-
 class SistemaRH:
+
+    def logar_acao(func):
+        def nova_funcaro(Funcionario):
+            print(f"Registrando bonus para um novo funcionario: {Funcionario.get_nome()}")
+            return func(Funcionario)
+
+    @logar_acao
     def mostrar_bonus(self, Funcionario):
-        print(f"Nome: ", Funcionario.get_nome(), "Bonus: ", Funcionario.calcular_bonus())
+        print(f"Nome: {Funcionario.get_nome()}, Bonus: {Funcionario.calcular_bonus()}")
