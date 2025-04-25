d4aaa4b9c741c563b357e7fd58fb06258817f162
Wendell <wendellbaresi93@gmail.com>
2025-04-25 15:33:11 -0300

    feat: Adiciona atributos da classe Funcionario

--- FuncionarioComum.py
+++ FuncionarioComum.py
@@ -2,6 +2,8 @@ from Funcionario import Funcionario
 
 
 class FuncionarioComum(Funcionario):
+    def __init__(self, nome, salario):
+        super().__init__(nome, salario)
 
     def calcular_bonus(self):
         return self.get_salario() * 0.1
