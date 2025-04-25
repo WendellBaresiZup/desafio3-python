5389af2c5e438e241fa20927a756f99e9ab9c028
Wendell <wendellbaresi93@gmail.com>
2025-04-25 15:37:54 -0300

    refactor: Altera metodo calcular_bonus

--- Gerente.py
+++ Gerente.py
@@ -4,7 +4,7 @@ from Funcionario import Funcionario
 class Gerente(Funcionario):
     def __init__(self, nome, salario, bonus_adicional):
         super().__init__(nome, salario)
-        self.__bonus.adicional = bonus_adicional
+        self.__bonus_adicional = bonus_adicional
 
     def calcular_bonus(self):
         return self.get_salario() * 0.2
